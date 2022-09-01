28b2dc6c4596539cdf4d8151eb445b77ba5b08c9
Miguel Carneiro <mcarneiromorenas@gmail.com>
2022-09-01 21:06:00 +0100

    Added current xp formatted & format

--- lua/codestats/curl.lua
+++ lua/codestats/curl.lua
@@ -1,18 +1,25 @@
 local function curl(payload)
-	local cmd = { 
-		'curl',
-		'--header', 'Content-Type: application/json',
-		'--header', 'X-API-Token: ' .. CODESTATS_API_KEY,
-		'--user-agent', 'codestats.nvim/' .. CODESTATS_VERSION,
-		'--data', payload,
-		'--request', 'POST',
-		'--silent',
-		'--output', '/dev/null',
-		'--write-out', '%{http_code}',
-		CODESTATS_API_URL .. '/my/pulses'
-	}
+    local cmd = {
+        "curl",
+        "--header",
+        "Content-Type: application/json",
+        "--header",
+        "X-API-Token: " .. CODESTATS_API_KEY,
+        "--user-agent",
+        "codestats.nvim/" .. CODESTATS_VERSION,
+        "--data",
+        payload,
+        "--request",
+        "POST",
+        "--silent",
+        "--output",
+        "/dev/null",
+        "--write-out",
+        "%{http_code}",
+        CODESTATS_API_URL .. "/my/pulses",
+    }
 
-	return vim.fn.system(cmd)
+    return vim.fn.system(cmd)
 end
 
 return curl
