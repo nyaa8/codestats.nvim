f7808024fbe09d84d37296a381a5e857167ecd47
Miguel Carneiro <mcarneiromorenas@gmail.com>
2022-09-05 18:22:30 +0100

    Luafy (#7)
    * reworked init file
    
    * Updated config
    
    * remove vimscript
    
    * fixed command
    
    * removed .vim
    
    * Revert "removed .vim"
    
    This reverts commit 170e1204d9f5ddc3935b3a92cc2fa7916c615f1c.
    
    * gone
    
    * augroups
    
    * Updated
    
    * rework
    
    * moved file to parent
    
    * RENAMED M
    
    * a
    
    * asd
    
    * empty
    
    * basic
    
    * revert
    
    * fix
    
    * imports
    
    * added config
    
    * groups and cmd
    
    * autocmd
    
    * added all methods back
    
    * updated cmd methods
    
    * typo
    
    * Update comment
    
    * removed buffer text
    
    * fixed indent

---
+++ lua/codestats.lua
@@ -0,0 +1,102 @@
+local curl = require("codestats.curl")
+local languages = require("codestats.languages")
+
+local M = {}
+
+local xp_table = {}
+local curr_xp = 0
+
+local base = {
+    version = "0.3.0",
+    url = "https://codestats.net/api",
+}
+
+M.gather_xp = function(filetype, xp_amount)
+    if filetype:gsub("%s+", "") == "" then
+        filetype = "plain_text"
+    end
+
+    xp_table[filetype] = (xp_table[filetype] or 0) + xp_amount
+    curr_xp = xp_table[filetype]
+end
+
+M.pulse = function()
+    if next(xp_table) == nil then
+        return
+    end
+
+    local time = os.date("%Y-%m-%dT%T%z")
+    local payload = '{ "coded_at": "' .. time .. '", "xps": ['
+    local xp_t = '{ "language": "%s", "xp": %d },'
+    local payload_end = "]}"
+
+    for filetype, xp in pairs(xp_table) do
+        payload = payload .. string.format(xp_t, languages[filetype] or filetype, xp)
+    end
+
+    payload = payload:sub(1, -2) .. payload_end
+
+    local response = curl(M.config.key, M.config.version, M.config.url, payload)
+
+    if response:sub(1, 1) == "2" then
+        xp_table = {}
+        curr_xp = 0
+    end
+end
+
+M.current_xp = function()
+    return curr_xp
+end
+
+M.current_xp_formatted = function()
+    return "CS::" .. tostring(curr_xp)
+end
+
+M.setup = function(options)
+    local codestats_api_key = vim.env.CODESTATS_API_KEY or options.key
+    if codestats_api_key == nil then
+        vim.cmd('echo "codestats.nvim: Please set $CODESTATS_API_KEY environment variable or set it in the config!"')
+        return
+    end
+    local username = vim.env.CODESTATS_USERNAME or options.username
+    if username == nil then
+        vim.cmd('echo "codestats.nvim: Please set $CODESTATS_USERNAME environment variable or set it in the config!"')
+        return
+    end
+
+    local opts = {
+        key = codestats_api_key,
+        username = username,
+    }
+
+    M.config = vim.tbl_extend("force", base, opts)
+
+    M.startup()
+end
+
+M.startup = function()
+    local codestats_group = vim.api.nvim_create_augroup("codestats", { clear = true })
+
+    vim.api.nvim_create_autocmd("VimLeavePre", {
+        group = codestats_group,
+        callback = function()
+            M.pulse()
+        end,
+    })
+
+    vim.api.nvim_create_autocmd({ "InsertCharPre", "TextChanged" }, {
+        group = codestats_group,
+        callback = function()
+            M.gather_xp(vim.api.nvim_buf_get_option(0, "filetype"), 1)
+        end,
+    })
+    -- Not sure if needed
+    --    vim.api.nvim_create_autocmd({ "BufWrite", "BufLeave" }, {
+    --        group = codestats_group,
+    --        callback = function()
+    --            M.pulse()
+    --        end,
+    --    })
+end
+
+return M
